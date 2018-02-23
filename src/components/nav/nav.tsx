bd3fa4e5f7f80489dc4c2b5fcb67ce10dc34f1e4
Jose Luis Sandoval <jl_sandoval_salas@hotmail.com>
2018-02-23 12:59:17 -0600

    add nav

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -4,7 +4,20 @@ import './nav.scss';
 class Nav extends React.Component{
     render(){
         return( 
-            <div className="jl-nav"></div>
+            <div className="jl-nav">
+                <div className="jl-nav__container_line">
+                    <div className="jl-nav__line">
+                        <div className="jl-nav__border__line"></div>
+                    </div>
+                    <div className="jl-nav__line">
+                        <div className="jl-nav__border__line"></div>
+                    </div>
+                    <div className="jl-nav__line">
+                        <div className="jl-nav__border__line"></div>
+                    </div>
+                    <div className="jl-nav__line"></div>
+                </div>
+            </div>
         )
     }
 }
