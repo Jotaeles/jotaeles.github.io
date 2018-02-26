3b554c9e30d8c10f8c71175b57f242eb82c4ea4b
Jose Luis Sandoval <jl_sandoval_salas@hotmail.com>
2018-02-26 11:40:40 -0600

    add animation title box

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { TweenMax, TweenLite, Cubic } from 'gsap';
+import { TweenMax, TweenLite, Cubic, Quart } from 'gsap';
 
 import * as $ from 'jquery';
 import './nav.scss';
@@ -15,12 +15,14 @@ class Nav extends React.Component<Props>{
     nav:JQuery<HTMLElement>;
     line:JQuery<HTMLElement>;
     close:JQuery<HTMLElement>;
-    link:JQuery<HTMLElement>;
+    title:JQuery<HTMLElement>;
+    box:JQuery<HTMLElement>;
     componentDidMount(){
         this.nav = $('.jl-nav');
         this.line = $('.jl-nav__line');
         this.close = $('.jl-nav__close');
-        this.link = $('.jl-nav__menu__link');
+        this.title = $('.jl-nav__menu__link__title');
+        this.box = $('.jl-nav__menu__title__box');
     }
 
     componentWillReceiveProps(nextProps : Props){
@@ -31,18 +33,24 @@ class Nav extends React.Component<Props>{
         this.nav.removeClass('is-open');
     }
     animationsComplete = () =>{
-        TweenMax.to(this.close, 0.5,{ opacity: 1});
-        TweenMax.to(this.link,0.5,{ opacity:1 });
+        TweenMax.to(this.close, 0.5, { opacity: 1});
+        TweenMax.to(this.box, 0.5, { x:'0%', onComplete:()=>{
+            TweenMax.to(this.box, 0.5, { delay:0.1, x:'105%', ease: Quart.easeOut});
+        },ease: Quart.easeIn });
+        TweenMax.delayedCall( 0.5,()=>{
+            TweenMax.to(this.title, 0.2, { opacity:1 });
+        });
     }
 
     handleAnimation = (isOpen:boolean)=>{
         if( isOpen ){
             this.nav.addClass('is-open');
-            TweenMax.staggerTo(this.line, 0.5,{ y:"0%", ease : 'Cubic.easeIn'},  0.25/2, this.animationsComplete);
+            TweenMax.staggerTo(this.line, 0.5, { y:"0%", ease : 'Cubic.easeIn'},  0.25/2, this.animationsComplete);
         }else{
 
-            TweenMax.to(this.close, 0.5,{ opacity: 0});
-            TweenMax.to(this.link,0.5,{ opacity:0 });
+            TweenMax.to(this.close, 0.5, { opacity: 0});
+            TweenMax.to(this.title,0.5, { opacity:0 });
+            TweenMax.set(this.box, { x:'-105%' });
             TweenMax.staggerTo(this.line, 0.5,{ y:"-100%", ease : 'Cubic.easeIn'},  0.25/2, this.allComplete);
         }
     }
@@ -69,9 +77,24 @@ class Nav extends React.Component<Props>{
                 </div>
                 <nav className="jl-nav__menu">
                     <ul className="jl-nav__menu__container">
-                        <li><a className="jl-nav__menu__link" href="#">Home</a></li>
-                        <li><a className="jl-nav__menu__link" href="#">About</a></li>
-                        <li><a className="jl-nav__menu__link" href="#">Contact</a></li>
+                        <li>
+                            <a className="jl-nav__menu__link">
+                                <span className="jl-nav__menu__link__title">Home</span>
+                                <span className="jl-nav__menu__title__box"></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a className="jl-nav__menu__link">
+                                <span className="jl-nav__menu__link__title">About</span>
+                                <span className="jl-nav__menu__title__box"></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a className="jl-nav__menu__link">
+                                <span className="jl-nav__menu__link__title">Contact</span>
+                                <span className="jl-nav__menu__title__box"></span>
+                            </a>
+                        </li>
                     </ul>
                 </nav>
             </div>
