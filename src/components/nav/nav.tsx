7ee6bbcd1af2079d6fe9271e47c668c1896fb7e2
Jose Luis Sandoval <jl_sandoval_salas@hotmail.com>
2018-03-26 16:11:01 -0600

    fix(app/nav): work in nav psd to html

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -18,15 +18,19 @@ class Nav extends React.Component<Props>{
     title:JQuery<HTMLElement>;
     subtitle:JQuery<HTMLElement>;
     box:JQuery<HTMLElement>;
-    link:JQuery<HTMLElement>;
+    link_menu:JQuery<HTMLElement>;
+    social_list:JQuery<HTMLElement>;
+    borderline:JQuery<HTMLElement>;
+    
     componentDidMount(){
         this.nav = $('.jl-nav');
         this.line = $('.jl-nav__line');
         this.close = $('.jl-nav__close');
-        this.link = $('.jl-nav__menu__link');
-        this.title = $('.jl-nav__menu__link__title');
+        this.link_menu = $('.jl-nav__menu__link');
         this.subtitle = $('.jl-nav__menu__link__subtitle');
         this.box = $('.jl-nav__menu__title__box');
+        this.social_list = $('.jl-social__list');
+        this.borderline = $('.jl-nav__border__line');
     }
 
     componentWillReceiveProps(nextProps : Props){
@@ -37,10 +41,12 @@ class Nav extends React.Component<Props>{
         this.nav.removeClass('is-open');
     }
     animationsComplete = () =>{
-        TweenMax.to(this.close, 0.5, { opacity: 1});
-        TweenMax.to(this.box, 0.4, { x:'0%' , onComplete:()=>{
-            TweenMax.to(this.link, 0.2, { y: 0, opacity: 1, ease: Cubic.easeOut});
-        }, ease: Cubic.easeOut});
+        TweenMax.staggerTo(this.borderline, 0.5,{ height:'100%', ease : 'Cubic.easeIn' }, 0.25/2 );
+        TweenMax.delayedCall(1,()=>{
+            TweenMax.to(this.close, 0.5, { opacity: 1});
+            TweenMax.to(this.link_menu, 0.5, { y: 0, opacity: 1, ease: Cubic.easeOut});
+            TweenMax.to(this.social_list, 0.5, { y: 0, opacity: 1, ease: Cubic.easeOut});
+        });
     }
 
     handleAnimation = (isOpen:boolean)=>{
@@ -50,9 +56,13 @@ class Nav extends React.Component<Props>{
         }else{
 
             TweenMax.to(this.close, 0.5, { opacity: 0});
-            TweenMax.to(this.link, 0.2, { y: 40, opacity: 0, ease: Cubic.easeIn});
+            TweenMax.to(this.link_menu, 0.5, { y: 40, opacity: 0, ease: Cubic.easeIn});
+            TweenMax.to(this.social_list, 0.5, { y: 40, opacity: 0, ease: Cubic.easeIn});
             
             TweenMax.delayedCall(0.5,()=>{
+                TweenMax.staggerTo(this.borderline, 0.5,{ height:'0%', ease : 'Cubic.easeIn' }, 0.25/2 );
+            });
+            TweenMax.delayedCall(1,()=>{
                 TweenMax.staggerTo(this.line, 0.5,{ y:"-100%", ease : 'Cubic.easeIn'},  0.25/2, this.closeMenu);
             });
         }
@@ -103,6 +113,13 @@ class Nav extends React.Component<Props>{
                         </li>
                     </ul>
                 </nav>
+                <div className="jl-social">
+                    <div className="jl-social__container">
+                        <div className="jl-social__list"><a className="jl-social__list__link" href="#">Instagram</a></div>
+                        <div className="jl-social__list"><a className="jl-social__list__link" href="#">LinkedIn</a></div>
+                        <div className="jl-social__list"><a className="jl-social__list__link" href="#">Facebook</a></div>
+                    </div>
+                </div>
             </div>
         )
     }
